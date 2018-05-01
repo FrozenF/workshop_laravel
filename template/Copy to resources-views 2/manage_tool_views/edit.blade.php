6a3f3abf05b3851d8d8d13500e991e6363ac8a65
Farih Nazihullah <farihnazihullah@gmail.com>
2018-05-01 21:58:58 +0700

    Laravel Workshop
    First Commint

---
+++ template/Copy to resources-views 2/manage_tool_views/edit.blade.php
@@ -0,0 +1,42 @@
+
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">Edit Tools</h1>
+    </div>
+    <!-- /.col-lg-12 -->
+</div>
+<!-- /.row -->
+<div class="row">
+    <div class="col-lg-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Edit Tools
+            </div>
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <form role="form">
+                            <div class="form-group">
+                                <label>Tool Name</label>
+                                <input class="form-control">
+                                <p class="help-block"></p>
+                            </div>
+                            <div class="form-group">
+                                <label>Tool Count</label>
+                                <input class="form-control">
+                                <p class="help-block"></p>
+                            </div>
+                            <button type="submit" class="btn btn-default">Submit Button</button>
+                            <button type="reset" class="btn btn-default">Reset Button</button>
+                        </form>
+                    </div>
+                </div>
+                <!-- /.row (nested) -->
+            </div>
+            <!-- /.panel-body -->
+        </div>
+        <!-- /.panel -->
+    </div>
+    <!-- /.col-lg-12 -->
+</div>
+            <!-- /.row -->
---
+++ template/Copy to resources-views 2/manage_tool_views/tambah.blade.php
@@ -0,0 +1,42 @@
+
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">Tambah Tools</h1>
+    </div>
+    <!-- /.col-lg-12 -->
+</div>
+<!-- /.row -->
+<div class="row">
+    <div class="col-lg-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Tambah Tools
+            </div>
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <form role="form">
+                            <div class="form-group">
+                                <label>Tool Name</label>
+                                <input class="form-control">
+                                <p class="help-block"></p>
+                            </div>
+                            <div class="form-group">
+                                <label>Tool Count</label>
+                                <input class="form-control">
+                                <p class="help-block"></p>
+                            </div>
+                            <button type="submit" class="btn btn-default">Submit Button</button>
+                            <button type="reset" class="btn btn-default">Reset Button</button>
+                        </form>
+                    </div>
+                </div>
+                <!-- /.row (nested) -->
+            </div>
+            <!-- /.panel-body -->
+        </div>
+        <!-- /.panel -->
+    </div>
+    <!-- /.col-lg-12 -->
+</div>
+            <!-- /.row -->
